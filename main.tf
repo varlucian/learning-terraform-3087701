8ec943bb75351ae6aa235a4f9a25e0725ab3c151
Lucian Varlan <varlucian@gmail.com>
2024-04-21 10:51:31 +0200

    Syntax fixes

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_instance" "blog" {
   instance_type          = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
-  subnet_id = modules.blog_vpc.public_subnets[0]
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "Learning Terraform"
@@ -51,7 +51,7 @@ module "blog_sg" {
   version = "5.1.2"
   name    = "blog"
 
-  vpc_id = modules.blog_vpc.vpc_id
+  vpc_id = module.blog_vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
