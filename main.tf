a4031f5c243cea313f2629d63d2df90e65c37f86
Lucian Varlan <varlucian@gmail.com>
2024-04-21 10:43:29 +0200

    Rename

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.2"
-  name    = "blog_new"
+  name    = "blog"
 
   vpc_id = data.aws_vpc.default.id
 
