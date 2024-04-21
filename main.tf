413983f3c0b64b1742dbd8fc0a74472026852634
Lucian Varlan <varlucian@gmail.com>
2024-04-21 10:58:50 +0200

    Load balancer

--- main.tf
+++ main.tf
@@ -46,6 +46,42 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
+
+  listeners = {
+    ex-http-listener = {
+      port     = 80
+      protocol = "HTTP"
+      target_group_index = 0
+    }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "blog"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+    }
+    targets = {
+      my_target = {
+        target_id = aws_instance.blog.id
+        port = 80
+      }
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.2"
