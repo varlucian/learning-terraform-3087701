8829855313362d4ad6112be56b6291814f7fe395
Lucian Varlan <varlucian@gmail.com>
2024-04-22 09:00:04 +0200

    fix: after wrong conflict resolution

--- main.tf
+++ main.tf
@@ -50,19 +50,27 @@ module "alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name            = "blog-alb"
-  vpc_id          = module.blog_vpc.vpc_id
-  subnets         = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id"]
+
 
   target_groups = [
-    ex-instance = {
-      name_prefix = "blog"
-      protocol    = "HTTP"
-      port        = 80
-      target_type = "instance"
-      target_id   = aws_instance.blog.id
-      port        = 80
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      }
     }
   ]
 
