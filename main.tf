6aa7348c8da45139681ec74f5c2e96e8c337597d
Lucian Varlan <varlucian@gmail.com>
2024-04-22 08:57:21 +0200

    fix

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ module "alb" {
   subnets         = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-  target_groups = {
+  target_groups = [
     ex-instance = {
       name_prefix = "blog"
       protocol    = "HTTP"
@@ -64,7 +64,7 @@ module "alb" {
       target_id   = aws_instance.blog.id
       port        = 80
     }
-  }
+  ]
 
   http_tcp_listeners = [
     {
