79242f3c53f1f37dd7734fb6c2a49cd107270255
Lucian Varlan <varlucian@gmail.com>
2024-05-03 18:46:00 +0200

    Name less than 6 characters

--- testing/main.tf
+++ testing/main.tf
@@ -2,7 +2,7 @@ module "testing" {
     source = "../modules/blog"
 
     environment = {
-        name           = "testing"
+        name           = "test"
         network_prefix = "10.1"
     }
 
