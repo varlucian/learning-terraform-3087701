3b2e8ef20ef24820d1f9a0da1cd32834e3b55e32
Lucian Varlan <varlucian@gmail.com>
2024-05-03 18:08:21 +0200

    Define testing environment

---
+++ testing/main.tf
@@ -0,0 +1,11 @@
+module "testing" {
+    source = "../modules/blog"
+
+    environment {
+        name           = "testing"
+        network_prefix = "10.1"
+    }
+
+    asg_min_size = 1
+    asg_max_size = 1
+}
---
+++ testing/output.tf
@@ -0,0 +1,3 @@
+output "environment_url" {
+    value = module.testing.environment_url
+}
